b9798226903f8283bd333afeacb435c29aa2814e
Victor Emanuel <victorer2@gmail.com>
2020-10-16 20:49:21 -0300

    Correcao no servidor de arquivo

--- Servidor de arquivos/clienteTCP.py
+++ Servidor de arquivos/clienteTCP.py
@@ -17,7 +17,4 @@ clientSocket.connect((serverName, serverPort))  # conecta o socket ao servidor
 
 sentence = input('Digite um comando: ')
 clientSocket.send(sentence.encode('utf-8'))  # envia o texto para o servidor
-modifiedSentence = clientSocket.recv(1024)  # recebe do servidor a resposta
-print('O servidor (\'%s\', %d) respondeu com: %s' %
-      (serverName, serverPort, modifiedSentence.decode('utf-8')))
 clientSocket.close()  # encerramento o socket do cliente
