352a2208d2e9077117d578667600c3a4d674788e
Victor Emanuel <victor.emanuel@esig.com.br>
2020-12-07 23:13:49 -0300

    adds keyboard listener

--- pyModbusTCP/Aval3/aval3.py
+++ pyModbusTCP/Aval3/aval3.py
@@ -1,4 +1,26 @@
 from pyModbusTCP.server import ModbusServer
+from pynput.keyboard import Listener, Key
+
+val1 = False
+
+
+def on_press(key):
+    global val1
+    print("Key pressed: {0}".format(key))
+    try:
+        if key.char == 'v':
+            val1 = not val1
+            print("val1: " + str(val1))
+    except:
+        print('caractere especial pressionado')
+
+
+# def on_release(key):
+    # print("Key pressed: {0}".format(key))
+# Create an instance of Listener
+with Listener(on_press=on_press) as listener:
+    listener.join()
+
 
 server = ModbusServer('127.0.0.1', port=502, no_block=True)
 
